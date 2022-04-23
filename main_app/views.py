8d7f30f7df82e16cd80d353c77b60f38e75d15f8
Christiano831 <cespinola@live.com>
2022-04-23 16:05:48 -0700

    Django setup

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+import re
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About Zoo Animals</h1>')
