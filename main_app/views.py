97fd9c286f42f582ee0c0fbc0e78a75ee5acf172
Christiano831 <cespinola@live.com>
2022-04-26 12:22:04 -0700

    add edit and delete

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 
 from .models import Animal
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 # Create your views here.
+
+class AnimalCreate(CreateView):
+  model = Animal
+  fields = '__all__'
+  success_url = '/animals/'
 
 def home(request):
   return render(request, 'home.html')
@@ -18,3 +25,11 @@ def animals_index(request):
 def info(request, animal_id):
   animal = Animal.objects.get(id=animal_id)
   return render(request, 'animals/info.html', {'animal': animal})
+
+class AnimalUpdate(UpdateView):
+  model = Animal
+  fields = ['name', 'species', 'gender', 'age']
+
+class AnimalDelete(DeleteView):
+  model = Animal
+  success_url = '/animals/'
