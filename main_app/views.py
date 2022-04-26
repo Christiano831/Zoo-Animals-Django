7bc2ca01c23eb331d1ebdf4c69d0a16b8d298edb
Christiano831 <cespinola@live.com>
2022-04-25 20:26:48 -0700

    add admin and info pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 import re
 from django.shortcuts import render
 
+from .models import Animal
+
 # Create your views here.
 
 def home(request):
@@ -10,17 +12,9 @@ def about(request):
     return render(request, 'about.html')
 
 def animals_index(request):
+  animals = Animal.objects.all()
   return render(request, 'animals/index.html', { 'animals': animals })
 
-class Animal:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, species, gender, age):
-    self.name = name
-    self.species = species
-    self.gender = gender
-    self.age = age
-
-animals = [
-  Animal('Kevin', 'rhino', 'male', 8),
-  Animal('Jasmine', 'zebra', 'female', 10),
-  Animal('Levi', 'monkey', 'male', 23)
-]
+def info(request, animal_id):
+  animal = Animal.objects.get(id=animal_id)
+  return render(request, 'animals/info.html', {'animal': animal})
