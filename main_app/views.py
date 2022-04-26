dc1f07a4ac2fc65f080a6c5871b12b7229d1c4d0
Christiano831 <cespinola@live.com>
2022-04-25 17:52:48 -0700

    Add home page and index page

--- main_app/views.py
+++ main_app/views.py
@@ -2,11 +2,25 @@ import re
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
+def animals_index(request):
+  return render(request, 'animals/index.html', { 'animals': animals })
+
+class Animal:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, species, gender, age):
+    self.name = name
+    self.species = species
+    self.gender = gender
+    self.age = age
+
+animals = [
+  Animal('Kevin', 'rhino', 'male', 8),
+  Animal('Jasmine', 'zebra', 'female', 10),
+  Animal('Levi', 'monkey', 'male', 23)
+]
